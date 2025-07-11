ff9c5efd95dd9686d9f73bd9f1c4cc1626be9286
pakowanko <wojtek@pakowanko.com>
2025-07-11 21:18:22 +0200

    FEAT: Dodano geokodowanie dla kodu pocztowego instalatora

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,4 @@
 // src/apiConfig.js
-const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://pv-service-backend-b0y53bb61027-europe-central2.run.app' 
-  : 'http://localhost:3000';
+const API_URL = '';
 
 export default API_URL;
