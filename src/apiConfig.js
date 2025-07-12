a768c88abe897483bfd3e6388f150abec45bf189
pakowanko <wojtek@pakowanko.com>
2025-07-12 15:04:04 +0200

    refactor: Poprawka CreateProfilePage

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,11 +1,5 @@
 // src/apiConfig.js
-
-// URL dla zapytań fetch (HTTP), które będą przekierowywane przez Firebase
-export const API_URL = ''; 
-
-// Pełny URL dla bezpośrednich połączeń (np. Socket.IO)
+const API_URL = 'https://pv-service-backend-1095388661827.europe-central2.run.app';
 export const SOCKET_URL = 'https://pv-service-backend-1095388661827.europe-central2.run.app';
 
-// Możemy też wyeksportować domyślnie API_URL dla uproszczenia
-const DEFAULT_URL = API_URL;
-export default DEFAULT_URL;
+export default API_URL;
