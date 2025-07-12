c4a21db42066d45969e47b8699103afede30331b
pakowanko <wojtek@pakowanko.com>
2025-07-12 16:16:44 +0200

    refactor: Poprawka wyswietlania zdjec

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,9 +1,13 @@
 // src/apiConfig.js
 
-// Pusty URL dla zapytań fetch (HTTP), które są przekierowywane przez Firebase
-export const API_URL = ''; 
+// Ten adres jest pusty, aby zapytania fetch (np. o listę profili) działały z Firebase rewrites
+export const API_URL = '';
 
-// Pełny, bezpośredni URL do Twojego backendu dla zasobów statycznych (obrazki) i WebSockets
-export const DIRECT_BACKEND_URL = 'https://pv-service-backend-1095388661827.europe-central2.run.app';
+// Ten adres jest pełny i służy do bezpośrednich połączeń, których nie obsługuje rewrite,
+// takich jak Socket.IO oraz ładowanie obrazków.
+const DIRECT_URL = 'https://pv-service-backend-1095388661827.europe-central2.run.app';
+
+export const SOCKET_URL = DIRECT_URL;
+export const DIRECT_BACKEND_URL = DIRECT_URL;
 
 export default API_URL;
